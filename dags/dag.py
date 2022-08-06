afd94a4f312ce3343d5f4641501323469c0d3d3a
hawkirk <harrison@hawkirk.com>
2022-08-06 17:27:19 -0500

    update dag and functions, docker-compose, remove xwalks

--- dags/dag.py
+++ dags/dag.py
@@ -5,15 +5,13 @@ from dag_functions.transform import transform_scorecard
 from dotenv import load_dotenv
 from datetime import datetime
 import time
+import glob
 import boto3
 import json
-import os 
+import os
 
 # top-level project directory
-proj_dir = os.getcwd()
-
-# specify data years to pull
-years = ['2014']
+proj_dir=os.getcwd()
 
 # get current date + time in a string
 timestamp = time.strftime("%Y%m%d-%H%M%S")
@@ -27,39 +25,53 @@ raw_output_path = proj_dir + '/data/raw/' + raw_file_name
 clean_output_path = proj_dir + '/data/clean/' + clean_file_name
 
 # wrapper to serialize extract_scorecard function output
-def download_api():
+def download_api(ti):
 
-    # run extraction function to pull data from API
-    in_data_raw = extract_scorecard(years)
+    # use XCOMS to pull return output from task 1
+    in_data_raw = ti.xcom_pull(task_ids='t1_extract_api')
 
     # serialize JSONL file to raw data folder
     with open(raw_output_path, 'w') as f:
         for row in in_data_raw:
             f.write(json.dumps(row) + '\n')
 
-# function for AWS S3 data upload
-def upload_s3(file_name, bucket, object_name):
+    return(raw_output_path)
 
-    # load AWS key
-    load_dotenv()
-    AWS_ACCESS_KEY_ID=os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
+# function for AWS S3 raw data upload
+def upload_raw_s3():
 
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    # identify the latest raw file
+    raw_files = glob.glob(proj_dir + '/data/raw/*') # * means all if need specific format then *csv.
+    latest_raw = max(raw_files, key=os.path.getctime)
 
-    s3.upload_file(file_name, bucket, object_name)
+    # upload to s3
+    s3_client = boto3.client('s3', aws_access_key_id = "", aws_secret_access_key = "")
+    s3_client.upload_file(latest_raw, 'college-scorecard-raw', raw_file_name)
+
+    return latest_raw
 
 # wrapper to serialize transform_scorecard function output to csv
-def write_clean_csv():
+def write_clean_csv(ti):
+
+    # use XCOMS to pull return output from task 1
+    latest_raw = ti.xcom_pull(task_ids='t3_upload_raw_s3')
 
     # run transform function to pull data from API
-    in_data_clean = transform_scorecard(file_path=raw_output_path)
+    in_data_clean = transform_scorecard(file_path=latest_raw)
 
     # convert df to csv
     in_data_clean.to_csv(clean_output_path, encoding='utf-8', index=False)
+
+# function for AWS S3 clean data upload
+def upload_clean_s3():
+
+    # identify the latest raw file
+    clean_files = glob.glob(proj_dir + '/data/clean/*') # * means all if need specific format then *csv.
+    latest_clean = max(clean_files, key=os.path.getctime)
+
+    # upload to s3
+    s3_client = boto3.client('s3', aws_access_key_id = "", aws_secret_access_key = "")
+    s3_client.upload_file(latest_clean, 'college-scorecard-clean', clean_file_name)
 
 # test call the functions
 # download_api()
@@ -80,44 +92,49 @@ def write_clean_csv():
 
 default_args = {
     'owner': 'airflow',
-    'retries': 1
+    'retries': 0
 }
 
 with DAG(
-    'scorecard_dag',
+    'scorecard_dag_39',
     default_args=default_args,
     start_date=datetime(2022, 1, 1),
     schedule_interval="@once",
-    catchup=True
+    catchup=False
 ) as dag:
 
     # task 1: pull data from College Scorecard API
     t1 = PythonOperator(
-        task_id='t1_extract_api',
+        task_id="t1_extract_api",
+        python_callable=extract_scorecard,
+        op_kwargs={'year': '2020', 'API_KEY': ''}
+    )
+
+    # task 2: persist output to disk
+    t2 = PythonOperator(
+        task_id='t2_serialize_output',
         python_callable=download_api
     )
 
-    # task 2: upload JSONL to 'college-scorecard-raw' S3 bucket
-    t2 = PythonOperator(
-        task_id='t2_upload_raw_s3',
-        python_callable=upload_s3,
-        op_args='college-scorecard-raw'
+    # task 3: upload JSONL to 'college-scorecard-raw' S3 bucket
+    t3 = PythonOperator(
+        task_id='t3_upload_raw_s3',
+        python_callable=upload_raw_s3
     )
 
-    # task 3: transform and export processed csv in '/data/clean/'
-    t3 = PythonOperator(
-        task_id="t3_transform_raw_to_clean",
+    # task 4: transform and export processed csv in '/data/clean/'
+    t4 = PythonOperator(
+        task_id="t4_transform_raw_to_clean",
         python_callable=write_clean_csv
     )
 
-    # task 4: upload transformed .CSV to 'college-scorecard-clean' S3 Bucket
-    t4 = PythonOperator(
-        task_id="t4_upload_clean_s3",
-        python_callable=upload_s3,
-        op_args='college-scorecard-clean'
+    # task 5: upload transformed .CSV to 'college-scorecard-clean' S3 Bucket
+    t5 = PythonOperator(
+        task_id="t5_upload_clean_s3",
+        python_callable=upload_clean_s3
     )
 
-    # task 5: load data to Redshift
+    # task 6: load data to Redshift
 
     # dag flow
-    t1 >> t2 >> t3 >> t4
+    t1 >> t2 >> t3 >> t4 >> t5
--- dags/dag_functions/transform.py
+++ dags/dag_functions/transform.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import json
-import csv
+import os
 
 def transform_scorecard(file_path):
     """Take JSONL, clean and transform, and spit out a csv."""
+
+    proj_dir=os.getcwd()
 
     # init list to store data
     in_data = []
@@ -88,12 +90,75 @@ def transform_scorecard(file_path):
     ]
     df_long[nan_to_zero] = df_long[nan_to_zero].fillna(0)
 
-    # load religious affiliation xwalk
-    religious_affil_dict = {}
-    relig_affil_xwalk_path = '/Users/h/Documents/code/college_scorecard_wh/data/xwalks/religious_affil_xwalk.csv'
-    with open(relig_affil_xwalk_path, mode='r') as xwalk:
-        reader = csv.reader(xwalk)
-        religious_affil_dict = {rows[0]:rows[1] for rows in reader}
+    religious_affil_dict = {
+        "0": "Secular",
+        "22": "American Evangelical Lutheran Church",
+        "24": "African Methodist Episcopal Zion Church",
+        "27": "Assemblies of God Church",
+        "28": "Brethren Church",
+        "30": "Roman Catholic",
+        "33": "Wisconsin Evangelical Lutheran Synod",
+        "34": "Christ and Missionary Alliance Church",
+        "35": "Christian Reformed Church",
+        "36": "Evangelical Congregational Church",
+        "37": "Evangelical Covenant Church of America",
+        "38": "Evangelical Free Church of America",
+        "39": "Evangelical Lutheran Church",
+        "40": "International United Pentecostal Church",
+        "41": "Free Will Baptist Church",
+        "42": "Interdenominational",
+        "43": "Mennonite Brethren Church",
+        "44": "Moravian Church",
+        "45": "North American Baptist",
+        "47": "Pentecostal Holiness Church",
+        "48": "Christian Churches and Churches of Christ",
+        "49": "Reformed Church in America",
+        "50": "Episcopal Church, Reformed",
+        "51": "African Methodist Episcopal",
+        "52": "American Baptist",
+        "53": "American Lutheran",
+        "54": "Baptist",
+        "55": "Christian Methodist Episcopal",
+        "57": "Church of God",
+        "58": "Church of Brethren",
+        "59": "Church of the Nazarene",
+        "60": "Cumberland Presbyterian",
+        "61": "Christian Church (Disciples of Christ)",
+        "64": "Free Methodist",
+        "65": "Friends",
+        "66": "Presbyterian Church (USA)",
+        "67": "Lutheran Church in America",
+        "68": "Lutheran Church - Missouri Synod",
+        "69": "Mennonite Church",
+        "71": "United Methodist",
+        "73": "Protestant Episcopal",
+        "74": "Churches of Christ",
+        "75": "Southern Baptist",
+        "76": "United Church of Christ",
+        "77": "Protestant, not specified",
+        "78": "Multiple Protestant Denomination",
+        "79": "Other Protestant",
+        "80": "Jewish",
+        "81": "Reformed Presbyterian Church",
+        "84": "United Brethren Church",
+        "87": "Missionary Church Inc",
+        "88": "Undenominational",
+        "89": "Wesleyan",
+        "91": "Greek Orthodox",
+        "92": "Russian Orthodox",
+        "93": "Unitarian Universalist",
+        "94": "Latter Day Saints (Mormon Church)",
+        "95": "Seventh Day Adventists",
+        "97": "The Presbyterian Church in America",
+        "99": "Other (none of the above)",
+        "100": "Original Free Will Baptist",
+        "101": "Ecumenical Christian",
+        "102": "Evangelical Christian",
+        "103": "Presbyterian",
+        "105": "General Baptist",
+        "106": "Muslim",
+        "107": "Plymouth Brethren",
+    }
 
     # map the religious affil xwalk to the column
     df_long["religious_affiliation"] = df_long["religious_affiliation"].astype(str).str[:-2]
